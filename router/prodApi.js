171355805a67e23ebca01bf43068bb3183ad5ca4
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-01-28 21:26:42 +0100

    Update prodApi.js

--- router/prodApi.js
+++ router/prodApi.js
@@ -77,13 +77,22 @@ router.get('/aaa', async (req, res) => {
     res.send(tab)
      
  });
-router.post('/zap', async (req, res) => {
-    const dataFromFacebook = req
-  // Handle the data and store it in MongoDB if needed
-  // ...
-   tab.push(dataFromFacebook)
- // res.status(200).json({ message: 'Data received successfully' });
-   await res.status(200).json(req.body);
+// router.post('/zap', async (req, res) => {
+//     const dataFromFacebook = req
+//   // Handle the data and store it in MongoDB if needed
+//   // ...
+//    tab.push(dataFromFacebook)
+//  // res.status(200).json({ message: 'Data received successfully' });
+//    await res.status(200).json(req.body);
+
+
+     
+//  });
+router.post('/zapp', async (req, res) => {
+  
+
+
+ res.status(200).json(req.body);
 
 
      
