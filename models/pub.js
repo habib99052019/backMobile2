90b305ff3cd4b373dfee4232ba27f6936651ef34
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-01-29 14:40:57 +0100

    Update pub.js

--- models/pub.js
+++ models/pub.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 const  pubSchema  = new mongoose.Schema({
    
-    title:String,
-    description:String,
-    images:[],
-    lien:String,
-    catigories:String
+    // title:String,
+    // description:String,
+    // images:[],
+    // lien:String,
+    // catigories:String
+     dynamicData: mongoose.Schema.Types.Mixed
   });
 module.exports=mongoose.model('pubSchema',pubSchema);
