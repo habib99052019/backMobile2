99e08cb9b51df02fadbe8c5baad79c340478bdb9
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-02-15 00:17:52 +0100

    Update landing.js

--- models/landing.js
+++ models/landing.js
@@ -4,8 +4,13 @@ const Schema = mongoose.Schema;
 
 const  landingSchema  = new mongoose.Schema({
    
-   title1:String,
+    name:String,
+  title1:String,
+  numberTitle1:String,
   title2:String,
+  numbertitle2:String,
+  endTitle2:String,
+  title1Section2:String,
   img1:String,
   video1:String,
   image2:String,
