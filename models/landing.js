a70a7d68ad26c48bf4001d88fcc991893ed67ec2
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-02-14 11:55:28 +0100

    Update landing.js

--- models/landing.js
+++ models/landing.js
@@ -1,1 +1,16 @@
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const  landingSchema  = new mongoose.Schema({
+   
+   title1:String,
+  title2:String,
+  img1:String,
+  video1:String,
+  image2:String,
+  option1:[]
+  
+    
+  });
+module.exports=mongoose.model(' landingSchema', landingSchema);
