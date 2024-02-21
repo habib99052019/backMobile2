e2e4dbbdcbdcc141cb6b941176a8048895e439d7
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-02-21 18:00:50 +0100

    Create rev.js

---
+++ models/rev.js
@@ -0,0 +1,10 @@
+rconst mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const  revSchema  = new mongoose.Schema({
+   
+  description:String,
+status:String,
+sender:[]
+  });
+module.exports=mongoose.model('revSchema',revSchema);
