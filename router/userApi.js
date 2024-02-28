7b8bc3f60d3b1f7a9cec417ac5f3671607acb9b8
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-02-28 16:51:16 +0100

    Update userApi.js

--- router/userApi.js
+++ router/userApi.js
@@ -12,7 +12,7 @@ const twilio = require('twilio');
 // Replace these variables with your own values from your Twilio account
 const accountSid = 'ACcf5c740f20c39ae33fddbd3915fec6c9';
 const authToken = '2f398191ab0674feb4739fd94db75bd8';
-const twilioPhoneNumber = '+21652307723';
+const twilioPhoneNumber = '+16206850366';
 const recipientPhoneNumber = '+21652560071'; // Replace with the recipient's phone number
 
 // Create a Twilio client
