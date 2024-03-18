19003bb81fbdacdd3d4f2817824110895f356c5d
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-03-18 04:51:40 +0100

    Update chat.js

--- models/chat.js
+++ models/chat.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const chatSchema  = new mongoose.Schema({
+    dateSending:String,
     userReceiv: {
             usid: String,
             username: String
