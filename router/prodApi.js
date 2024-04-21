cec4f7743a9952a3b4e615cb82ee7dcb53bcd79d
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-04-21 12:08:23 +0200

    Update prodApi.js

--- router/prodApi.js
+++ router/prodApi.js
@@ -78,12 +78,7 @@ function haversineDistance(lat1, lon1, lat2, lon2) {
               Math.sin(dLon/2) * Math.sin(dLon/2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     const distance = R * c;
-    if(distance <16){
-      return  false
-    }
-    else {
-       return true; 
-    }
+   return distance
     
 }
 
