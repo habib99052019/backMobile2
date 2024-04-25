3fc95bf3982d1f73e83bf5b22b727a4fef5b5295
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-04-26 00:21:45 +0200

    Update pubApi.js

--- router/pubApi.js
+++ router/pubApi.js
@@ -27,7 +27,7 @@ async function sendWhatsAppMessage(to, message) {
   try {
     const response = await client.messages.create({
       body: message,
-      from: 'whatsapp:+14155238886', // Your Twilio Sandbox Number +12512206076 +14155238886
+      from: 'whatsapp:+14792822034', // Your Twilio Sandbox Number +12512206076 +14155238886
       to: `whatsapp:${to}`,
     });
     console.log(`Message sent to ${to}: ${response.sid}`);
