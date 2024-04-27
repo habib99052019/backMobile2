d85e6bc1c66c8fa3be58da0086ce6b4ddc603dc3
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-04-27 17:00:11 +0200

    Update pubApi.js

--- router/pubApi.js
+++ router/pubApi.js
@@ -29,7 +29,7 @@ router.get('/scrape', async (req, res) => {
         const products = [];
 
         // Récupérer les éléments qui contiennent les informations des produits
-        $('.c4fc20ba').each((index, element) => {
+        $('.f343d9ce').each((index, element) => {
             // const name = $(element).find('.listing-title').text().trim();
             const price = $(element).find('.f343d9ce').text().trim();
             // const location = $(element).find('.listing-location').text().trim();
